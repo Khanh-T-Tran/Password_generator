144defa536fa76bf6941b54bcac1efbfb5df417c
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-05-14 00:54:43 -0700

    complete the function and loop

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ var cflc; // confirm lower case
 var cfuc; // confirm upper case
 var cfnum; // confirm number
 var cfSpec; // confirm special characters
-var userChoices
+var userChoices;
 
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -29,14 +29,16 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// This is the function to prompt the passwork
 function generatePassword() {
-  pswl = prompt('Your password must have at least one lower case, one upper case, one number, and one special character!');
+  pswl = prompt('Your password must have:\n - The length between 8 and 128 characters.\n - One lower case.\n - One upper case.\n - One number.\n - One special character.\n   * Please input the length of your password below by number:');
   console.log("Password length" + pswl);
   
   if(!pswl) {
     alert("Required value");
 
-  } else if (pswl < 8 || pswl > 128) {
+  } 
+  else if (pswl < 8 || pswl > 128) {
     pswl = prompt('You must choose between 8 and 128 characters');
     console.log('Password length' + pswl);
   
@@ -51,11 +53,89 @@ function generatePassword() {
     console.log('Special character' + cfSpec);
   };
 
-  // if (!cflc && !cfuc && !cfnum && !cfSpec) {
-  //   userChoices = alert('Please choose your password accoring to the criteria!');
+  if (!cflc && !cfuc && !cfnum && !cfSpec) {
+    userChoices = alert('Please choose your password accoring to the criteria!');
+
+  } else if (cflc && cfuc && cfnum && cfSpec) {
+    userChoices = lower.concat(upper, numbers, special);
+    console.log(userChoices);
 
   
+  } else if (cflc && cfuc && cfnum) {
+    userChoices = lower.concat(upper, numbers);
+    console.log(userChoices);
+  }
+  else if (cflc && cfuc && cfSpec) {
+    userChoices = lower.concat(upper, special);
+    console.log(userChoices);
+  }
+  else if (cflc && cfnum && cfSpec) {
+    userChoices = lower.concat(numbers, special);
+    console.log(userChoices);
+  }
+  else if (cfuc && cfnum && cfSpec) {
+    userChoices = upper.concat(numbers, special);
+    console.log(userChoices);
+  }
+  
 
+  else if (cflc && cfuc) {
+    userChoices = lower.concat(upper);
+    console.log(userChoices);
+  }
+  else if (cflc && cfnum) {
+    userChoices = lower.concat(numbers);
+    console.log(userChoices);
+  }
+  else if (cflc && cfSpec) {
+    userChoices = lower.concat(special);
+    console.log(userChoices);
+  }
+  else if (cfuc && cfnum) {
+    userChoices = upper.concat(numbers);
+    console.log(userChoices);
+  }
+  else if (cfuc && cfSpec) {
+    userChoices = upper.concat(special);
+    console.log(userChoices);
+  }
+  else if (cfnum && cfSpec) {
+    userChoices = numbers.concat(special);
+    console.log(userChoices);
+  }
+ 
 
+  else if (cflc) {
+    userChoices = lower;
+    console.log(userChoices);
+  }
+  else if (cfuc) {
+    userChoices = blankUpper.concat(upper);
+    console.log(userChoices);
+  }
+  else if (cfnum) {
+    userChoices = numbers;
+    console.log(userChoices);
+  }
+  else if (cfSpec) {
+    userChoices = special;
+    console.log(userChoices);
+  };
 
+  
+  var pwBlank = [];
+  
+  
+  for (var i = 0; i < pswl; i++) {
+    var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+    pwBlank.push(allChoices);
+    console.log(allChoices);
+  }
+  
+  
+  var password = pwBlank.join("");
+  console.log("Your Pasword is: " + password);
+  return password;
+  
 }
+
