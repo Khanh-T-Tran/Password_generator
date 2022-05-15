561a5b27a1ca06761b2cc23a455fa89ba613c6bd
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-05-14 23:16:52 -0700

    refactor and comment

--- Develop/script.js
+++ Develop/script.js
@@ -31,106 +31,93 @@ generateBtn.addEventListener("click", writePassword);
 
 // This is the function to prompt the passwork
 function generatePassword() {
-  pswl = prompt('Your password must have:\n - The length between 8 and 128 characters.\n - One lower case.\n - One upper case.\n - One number.\n - One special character.\n   * Please input the length of your password below by number:');
+  pswl = parseInt(prompt('Your password must have:\n - The length between 8 and 128 characters.\n - One lower case.\n - One upper case.\n - One number.\n - One special character.\n   * Please input the length of your password below by number:'));
   console.log("Password length" + pswl);
   
+  // If the password length is not meeting the condition, -> alert, then pop message for length condition , if pswl is meeting the condition,  user will choose the criteria
   if(!pswl) {
     alert("Required value");
 
   } else if (pswl < 8 || pswl > 128) {
-    pswl = prompt('You must choose between 8 and 128 characters');
-    console.log('Password length' + pswl);
-  
+    pswl = parseInt(prompt("You must choose between 8 and 128"));
+    
   } else {
    
     cflc = confirm("Will this password contain lower case letters?");
-    console.log('Lower case' + cflc);
+    
     cfuc = confirm('Will this password contain upper case letter?');
-    console.log('Upper case' + cfuc);
+    
     cfnum = confirm('Will password this contain number?');
-    console.log('Number' + cfnum);
+    
     cfSpec = confirm('Will this password contain special characters?');
-    console.log('Special character' + cfSpec);
+    
   
   }; 
 
  
-   
-  if (!cflc && !cfuc && !cfnum && !cfSpec) {
+   // If non of the criteria were confirmed -> alert the message, ortherwise will filter out by user selection
+  
+   if (!cflc && !cfuc && !cfnum && !cfSpec) {
     userChoices = alert('Please choose your password accoring to the criteria!');
 
   } else if (cflc && cfuc && cfnum && cfSpec) {
     userChoices = lower.concat(upper, numbers, special);
-    console.log(userChoices);
   
   } else if (cflc && cfuc && cfnum) {
     userChoices = lower.concat(upper, numbers);
-    console.log(userChoices);
   
   } else if (cflc && cfuc && cfSpec) {
     userChoices = lower.concat(upper, special);
-    console.log(userChoices);
   
   } else if (cflc && cfnum && cfSpec) {
     userChoices = lower.concat(numbers, special);
-    console.log(userChoices);
   
   } else if (cfuc && cfnum && cfSpec) {
     userChoices = upper.concat(numbers, special);
-    console.log(userChoices);
   
   } else if (cflc && cfuc) {
     userChoices = lower.concat(upper);
-    console.log(userChoices);
   
   } else if (cflc && cfnum) {
     userChoices = lower.concat(numbers);
-    console.log(userChoices);
   
   } else if (cflc && cfSpec) {
     userChoices = lower.concat(special);
-    console.log(userChoices);
   
   } else if (cfuc && cfnum) {
     userChoices = upper.concat(numbers);
-    console.log(userChoices);
   
   } else if (cfuc && cfSpec) {
     userChoices = upper.concat(special);
-    console.log(userChoices);
   
   } else if (cfnum && cfSpec) {
     userChoices = numbers.concat(special);
-    console.log(userChoices);
   
   } else if (cflc) {
     userChoices = lower;
-    console.log(userChoices);
   
   } else if (cfuc) {
     userChoices = blankUpper.concat(upper);
-    console.log(userChoices);
   
   } else if (cfnum) {
     userChoices = numbers;
-    console.log(userChoices);
   
   } else if (cfSpec) {
     userChoices = special;
-    console.log(userChoices);
+ 
   };
 
 
-  var Blank = [];
+  var Blank = []; // Emty variable for input user password
   var combinePW;
   
+  // CombinePW will be picked up randomly according to userChoises
   for (var i = 0; i < pswl; i++) {
     combinePW = userChoices[Math.floor(Math.random() * userChoices.length)];
     Blank.push(combinePW);
-    console.log(combinePW);
   }
   
-  
+  // Join the array to the string
   var password = Blank.join("");
   console.log("Your Pasword is: " + password);
   return password;
