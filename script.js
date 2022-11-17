53513d571462a9b4da19da53297cc47f5d124612
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-11-16 17:29:15 -0800

    simplify the app

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 // First we select generate button by targeting the btn class
-const generateBtn = document.getElementById("#btn");
+const generateBtn = document.getElementById("btn");
 
 // Then we set password parameters for each element
-const length = document.getElementById('#characterNumber');
-const upperCase = document.getElementById('#includeUppercase');
-const numbers = document.getElementById('#includeNumbers');
-const symbols = document.getElementById('#includeSymbols');
+const characterRange = document.getElementById('characterRange');
+const characterNumber = document.getElementById('characterNumber');
 
-// create arrays to hold password parameters
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+// create arrays to hold password parameters based on ASCii table: https://www.ascii-code.com/
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
 
-// This for prevent the page from refresh every time the submit event happens then call the function generating password
-const form = document.getElementById('pwGeneratorForm')
-passwordDisplay = document.getElementById('passwordDisplay')
+// this part sync Range and Number of password input
+characterNumber.addEventListener('input', syncCharacterLength)
+characterRange.addEventListener('input', syncCharacterLength)
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  const pwLength = length.value;
-  const includeUpperCase = upperCase.checked;
-  const includeNumbers = numbers.checked;
-  const includeSymbols = symbols.checked;
-
-  const password = generatePassword(pwLength, includeUpperCase, includeNumbers, includeSymbols);
-  passwordDisplay.innerText = password
-})
-
-
-// Write password to the #password-display input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.getElementById("#passwordDisplay");
-
-//   passwordText.value = password;
-// }
+function syncCharacterLength(e) {
+  const value = e.target.value
+  characterNumber.value = value
+  characterRange.value = value
+}
 
 // generate array from low to high
 function arrayFromLowToHigh(low, high) {
@@ -45,16 +29,24 @@ function arrayFromLowToHigh(low, high) {
   }
   return array;
 }
+
+// This for prevent the page from refresh every time the submit event happens then call the function generating password
+const form = document.getElementById('pwGeneratorForm')
+passwordDisplay = document.getElementById('passwordDisplay')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const pwLength = characterNumber.value;
+  const password = generatePassword(pwLength);
+  passwordDisplay.innerText = password
+})
+
 // logic for generating password
-function generatePassword(pwLength, pwUpperCase, pwNumbers, pwSymbols) {
-  let charCodes = LOWERCASE_CHAR_CODES
-  if(includeUpperCase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if(includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-  if(includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+function generatePassword(pwLength) {
+  let charCodes = LOWERCASE_CHAR_CODES.concat(UPPERCASE_CHAR_CODES, SYMBOL_CHAR_CODES, NUMBER_CHAR_CODES)
 
   const passwordCharacters = [];
-  for(let i = 0; i < pwLength; i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * pwLength)]
+  for (let i = 0; i < pwLength; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
   return passwordCharacters.join("")
